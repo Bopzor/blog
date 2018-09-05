9796ae8d5735a8b933a07bfc3c1c90586c3d4130
Bopzor <violaine.ballandin@gmail.com>
2018-09-05 23:00:59 +0200

    fix redirect

--- web/src/components/article.js
+++ web/src/components/article.js
@@ -56,7 +56,7 @@ const Article = (props) => {
 
         <Admin>
           <Link
-            to={{ pathname: `/blog` }}
+            to={{ pathname: `/` }}
             className="link"
           >
             <i
